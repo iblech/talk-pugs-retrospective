93afbb680944fea7cf089d754a16900eab8e84d9
Ingo Blechschmidt <iblech@web.de>
2015-05-01 21:07:35 +0200

    Some more notes and links

---
+++ code-snippets/day1-main.hs
@@ -0,0 +1,15 @@
+-- src/Main.hs at revision 1
+{-# OPTIONS -fglasgow-exts #-}                                                                    
+                                                                                                  
+{-                                                                                                
+    The Main REPL loop.                                                                           
+                                                                                                  
+    A ship then new they built for him                                                            
+    Of mithril and of elven-glass                                                                 
+    With shining prow; no shaven oar                                                              
+    Nor sail she bore on silver mast;                                                             
+    The Silmaril as lantern light                                                                 
+    And banner bright with living flame                                                           
+    To gleam thereon by Elbereth                                                                  
+    Herself was set, who thither came...                                                          
+-}                                                                                                
--- code-snippets/day108-net-irc.pm
+++ code-snippets/day108-net-irc.pm
@@ -1,7 +1,7 @@
 # ext/Net-IRC/lib/Net/IRC.pm at revision 2850
 module Net::IRC-0.03;
 
-sub new_bot(Str $nick, Str $host, ...) {
+sub new_bot (Str $nick, Str $host, ...) {
     my $self;
     ...;
     return $self = {
